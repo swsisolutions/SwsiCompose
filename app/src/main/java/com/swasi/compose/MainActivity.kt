fdfb87018d1fad967cd2dc8e10f1b7257efd4511
siba-x-prasad <siba.x.prasad@gmail.com>
2025-08-30 15:52:22 +0100

    Fixed jitpack error

--- app/src/main/java/com/swasi/compose/MainActivity.kt
+++ app/src/main/java/com/swasi/compose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.swasi.compose.ui.theme.SwsiComposeTheme
+import com.swasi.composeuikit.AppButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +34,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Row {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        AppButton(onClick = {})
+    }
 }
 
 @Preview(showBackground = true)
